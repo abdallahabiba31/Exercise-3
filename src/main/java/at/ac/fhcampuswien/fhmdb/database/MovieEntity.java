204dd88b34d8717fbed1bfb5a0587238dd5c3893
sabineberger13 <sabineberger1301@gmail.com>
2023-05-04 18:03:07 +0200

    einige Tabellen angelegt

--- src/main/java/at/ac/fhcampuswien/fhmdb/database/MovieEntity.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/database/MovieEntity.java
@@ -1,10 +1,33 @@
 package at.ac.fhcampuswien.fhmdb.database;
 
+import at.ac.fhcampuswien.fhmdb.models.Genre;
+import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
-@DatabaseTable ()
+import java.util.ArrayList;
+import java.util.List;
 
-public class MovieEntity {
+@DatabaseTable (tableName = "movie")
+
+    public class MovieEntity {
+    @DatabaseField(generatedId = true)
+    private long id;
+    @DatabaseField()
+    private String title;
+    @DatabaseField()
+    private String description;
+
+    //@DatabaseField()
+   // private final List<Genre> genres;
+
+    @DatabaseField()
+    private int releaseYear;
+    @DatabaseField()
+    private String imgUrl;
+    @DatabaseField()
+    private int lengthInMinutes; // in minutes
+    @DatabaseField()
+    private double rating; // 0-10
 
 
 }
