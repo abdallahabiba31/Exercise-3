57f4ce19a44028201f4f68def7a60b9bfe8a3c74
abdallahabiba31 <abdallahabiba97@gmail.com>
2023-05-06 20:27:58 +0200

    added watchlist-view.fxml and WatchlistViewController

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/controller/WatchlistViewController.java
@@ -0,0 +1,8 @@
+package at.ac.fhcampuswien.fhmdb.controller;
+
+import javafx.event.ActionEvent;
+
+public class WatchlistViewController {
+    public void loadHomeView(ActionEvent actionEvent) {
+    }
+}
